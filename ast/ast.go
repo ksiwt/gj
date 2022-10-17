e7c997f935b223029aa77e2d7817928332d6365f
pohedev <ksiwt.jp@gmail.com>
2022-10-17 12:49:27 +0900

    add comment on ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,20 @@
 package ast
 
+// RootNodeType identifies the type of JSON RootNode.
 type RootNodeType int
 
 const (
-	RootNodeTypeObject RootNodeType = iota + 1
-	RootNodeTypeArray
+	RootNodeTypeObject RootNodeType = iota + 1 // object
+	RootNodeTypeArray                          // array
 )
 
+// RootNode represents a what JSON starts every parsed AST.
+type RootNode struct {
+	RootNodeType
+	*Value
+}
+
+// LiteralType identifies the type of JSON Literal.
 type LiteralType int
 
 const (
@@ -17,6 +25,13 @@ const (
 	LiteralTypeFalse
 )
 
+// Literal represents a a JSON literal.
+type Literal struct {
+	LiteralType
+	Val any
+}
+
+// State identifies the type of parsing JSON state.
 type State int
 
 const (
@@ -24,53 +39,50 @@ const (
 	StateObjectOpen
 	StateObjectProperty
 	StateObjectComma
+
 	StatePropertyStart
 	StatePropertyKey
 	StatePropertyColon
 	StatePropertyValue
+
 	StateArrayStart
 	StateArrayOpen
 	StateArrayValue
 	StateArrayComma
 )
 
-type RootNode struct {
-	RootNodeType
-	*Value
-}
-
+// Object represents a JSON object.
 type Object struct {
 	Children []Property
 	Start    int
 	End      int
 }
 
+// Property represents a JSON object property.
 type Property struct {
 	Identifier
 	Value any
 }
 
+// Identifier represents a key identifier of JSON object property.
 type Identifier struct {
 	Value string
 }
 
+// Array represents a JSON array.
 type Array struct {
 	Children []ArrayItem
 	Start    int
 	End      int
 }
 
+// ArrayItem represents a value of JSON array.
 type ArrayItem struct {
 	Value any
 }
 
+// Value represents a value of JSON value
+// (object | array | boolean | string | number | null).
 type Value struct {
 	Value any
 }
-
-type Literal struct {
-	LiteralType
-	Val any
-}
-
-type ValueSet interface{}
